c3c58628e4420de4ce1af59ec64737e176d98796
awadhwana <24ankitw@gmail.com>
2018-11-19 13:44:20 +0530

    Stock search added with validations

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,5 +1,17 @@
 # stocks controler
 class StocksController < ApplicationController
   def search
+    if params[:stock].present?
+      @stock = Stock.new_from_lookup(params[:stock])
+      if @stock.present?
+        render 'users/my_portfolio'
+      else
+        flash[:danger] = 'Invalid name'
+        redirect_to my_portfolio_url
+      end
+    else
+      flash[:danger] = 'Stock name cannot be empty'
+      redirect_to my_portfolio_url
+    end
   end
 end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,12 @@
+# stock model
 class Stock < ApplicationRecord
+  def self.new_from_lookup(ticker)
+    begin
+      looked_up_stock = StockQuote::Stock.quote(ticker)
+      new(name: looked_up_stock.company_name, ticker: looked_up_stock.symbol,
+          last_price: looked_up_stock.latest_price)
+    rescue StandardError
+      return nil
+    end
+  end
 end
